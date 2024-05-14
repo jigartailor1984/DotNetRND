f501c2bc3079091fa52be9bd5052548ace51a6ca
Jigar Tailor <jigar_tailor@persistent.com>
2024-05-14 13:12:51 +0530

    added logic to browse directory

--- Web/FileShareWebApp/Program.cs
+++ Web/FileShareWebApp/Program.cs
@@ -1,5 +1,25 @@
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
+IWebHostEnvironment env=builder.Environment;
 var app = builder.Build();
+var protoProvider = new FileExtensionContentTypeProvider();
+    protoProvider.Mappings.Clear();
+    protoProvider.Mappings[".proto"] = "text/plain";
+
+app.UseStaticFiles(new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "Share\\Protos")),
+        RequestPath = "/Share/protos",
+        ContentTypeProvider = protoProvider
+
+    });
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "Share")),
+    RequestPath = "/share"
+});
 
 app.MapGet("/", () => "Hello World!");
 
